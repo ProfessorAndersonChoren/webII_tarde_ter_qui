c2f7bc2faa8003cfe1b0c82fab38dc6927e50f4f
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-06-23 14:04:08 -0300

    Inclusão do Composer

---
+++ loja_de_varejo/src/View/form_add_product.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de varejo - Cadastro de produtos</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="bg-blue-400">
+            <ul class="flex">
+                <li class="mr-3">
+                    <a href="../../index.html">Home</a>
+                </li>
+                <li>
+                    <a href="#">Cadastro de produtos</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
