d25c5e55dd2f8a0000f4810d4ba2586737a826ec
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-07-21 14:15:03 -0300

    Loja de varejo - Script de criação

--- loja_de_varejo/script_criacao.sql
+++ loja_de_varejo/script_criacao.sql
@@ -2,6 +2,7 @@ create database if not exists retail_store_tuesday;
 use retail_store_tuesday;
 create table if not exists product(
 	product_code smallint primary key auto_increment,
+	product_barcode char(13) not null,
 	product_name varchar(30) not null,
 	product_price decimal(10,2) not null,
 	product_quantity smallint not null,
